b8cbbb95a4dddd2c26e7ec89dd0767dd56559c28
Rizwan <152837388+M-Rizwan86@users.noreply.github.com>
2025-07-21 16:18:19 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
-let fruits = ["Mango", "banana", "Aam", "Amb"];
-function xyz() {
-    let list = "";
-    
-    for (let i = 0; i < fruits.length; i++) {
-        list += "<li>" + fruits[i] + "</li>";
-    }
-    document.getElementById("fruits").innerHTML = list;
-}
+async function getData(){
+    response = await fetch("https://dummyjson.com/products")
+    data = await response.json()
+    card = document.getElementById('cardid')
+    data.products.map(({title, price, description, rating, images ,reviews })=>{
+        card.innerHTML  += `  <div class="product-card">
+    <div class="product-image">
+      <img src="${images[0]}" alt="${title}">
+    </div>
+    <div class="product-content">
+      <h3 class="product-title">${title}</h3>
+      <p class="product-description">${description}</p>
+      <div class="product-price-rating">
+        <span class="product-price">$${price}</span>
+        <span class="product-rating">${rating} ⭐</span>
+      </div>
+      <p class="product-reviews">${reviews?.length || 0} reviews</p>
+      <button class="btn btn-primary btn-block">Add to Cart</button>
+    </div>
+  </div>`
+    })
 
-function xyz2(){
-var prompt1=prompt("type any fruit name")
-fruits.push(prompt1)
-    let list = "";
-    
-    for (let i = 0; i < fruits.length; i++) {
-        list += "<li>" + fruits[i] + "</li>";
-    }
-    document.getElementById("fruits").innerHTML = list;
+
+
 }
+getData()
